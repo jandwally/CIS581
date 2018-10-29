4b250955aad77e97f3505c9dc709a76ce0cf42c7
jandwally <jwalli@sas.upenn.edu>
2018-10-29 19:02:09 -0400

    Sample images

--- stitching.py
+++ stitching.py
@@ -1,2 +1,22 @@
 
-# stitching.py
+'''
+  File name: stitching.py
+'''
+
+# libs
+import numpy as np
+import matplotlib.pyplot as plt
+
+# files
+
+
+# Image blending script:
+def stitching():
+
+    # corner_detector
+    
+
+if __name__ == "__main__":
+    stitching()
+
+
