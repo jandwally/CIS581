2c0375616731ebf69b5c93ad34764a7988e930ed
jandwally <jwalli@sas.upenn.edu>
2018-10-30 19:38:42 -0400

    feat-desc

--- anms.py
+++ anms.py
@@ -8,13 +8,15 @@
   File clarification:
     Implement Adaptive Non-Maximal Suppression. The goal is to create an uniformly distributed 
     points given the number of feature desired:
-    - Input cimg: H × W matrix representing the corner metric matrix.
+    - Input corner_matrix: H × W matrix representing the corner metric matrix.
     - Input max_pts: the number of corners desired.
     - Outpuy x: N × 1 vector representing the column coordinates of corners.
     - Output y: N × 1 vector representing the row coordinates of corners.
     - Output rmax: suppression radius used to get max pts corners.
 '''
 
-def anms(cimg, max_pts):
-  # Your Code Here
+def anms(corner_matrix, max_pts):
+
+  
+
   return x, y, rmax
