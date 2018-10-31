7155de019570dcfbed5bc651c449e08610d5fefe
mchiquier <mchiquier@gmail.com>
2018-10-31 16:06:47 -0700

    updates on feat_match.py

--- feat_match.py
+++ feat_match.py
@@ -18,14 +18,30 @@ import numpy as np
 
 def feat_match(descs1, descs2):
   h,w= descs1.shape[0:2]
+  pairs = []
   b = np.zeros([h*w,h,w])
-  for i in range(h):
-    for j in range(w):
-      curr = np.empty([h,w])
-      curr.fill(descs2[i,j])
-      b[(i*h+j),:,:] = curr
-  matched = descs1[np.newaxis,:]-b[:,np.newaxis]
-  return match
+
+  for i in range(descs2.shape[1]):
+    d1i = descs2[:,i]
+    #maybe np.subtract
+    dist = np.sum(np.square(descs2-d1i), axis = 0) 
+    dist.sort()
+    best = dist[0]
+    index_of_best = np.where(dist == best)
+    sbest = dist[1]
+    index_of_second_best = np.where(dist == sbest)
+    d2i = desc2[index_of_best]
+    sdist = np.sum(np.square(dist1-d2i), axis = 0)
+    sdist.sort()
+    pbest = sdist[0]
+    psbest = sdist[1]
+    ratio = best/pbest
+    ratios = sbest/psbest
+
+    if ratio < 0.7 and ratios < 0.7:
+        #take index_of_best, index_of_second_best,
+      pairs.append(index_of_best, index_of_second_best)
+    return match
 
 a= np.array([[5,4,3],[10,9,1]])
 c = np.array([[1,2,3],[4,5,6]])
