4d7f9fdbc29b64fc7a293a697c1d7076bb46688c
jandwally <jwalli@sas.upenn.edu>
2018-10-31 21:08:24 -0400

    Wrote anms (not completely vectorized yet), and updated test file to call it

--- ransac_est_homography.py
+++ ransac_est_homography.py
@@ -15,6 +15,38 @@
     - Output inlier_ind: N × 1 vector representing if the correspondence is inlier or not. 1 means inlier, 0 means outlier.
 '''
 
+from est_homography import est_homography
+
 def ransac_est_homography(x1, y1, x2, y2, thresh):
-  # Your Code Here
+  n = x1.shape[0]
+
+  def ransac():
+
+    ''' 1. Randomly select 4 points '''
+    rands = np.random.permutation(n)[0:4]
+    rand_points1 = x1[rands], y1[rands]
+    rand_points2 = x2[rands], y2[rands]
+
+    est_homo = est_homography(x, y, X, Y)
+
+  '''
+  We use RANSAC to pull out a minimal set of feature matches, estimate the homography and then
+  count the number of inliers that agree with the current homography estimate. After repeated trials, the
+  homography estimate with the largest number of inliers is used to compute a least squares estimate
+  for the homography, which is then returned in the homography estimate H.
+  H is a 3×3 matrix with 8 degrees of freedom. You need to solve a system of at least 8 linear equations
+  to solve for the 8 unknowns of H. These 8 linear equations are based on the 4 pairs of corresponding
+  points.
+  Recall RANSAC:
+  1. Randomly select four feature pairs
+  2. Compute the homography relating the four selected matches with the function est_homography.m
+  3. Compute the number of inliers to count (SSD distance after applying the estimated homography
+  below the threshold thresh) how many matches agree with this estimate. Don’t forget to create
+  inlier_ind
+  4. Repeat the above random selection nRANSAC times and keep the estimate with the largest number
+  of inliers
+  5. Computes the least squares estimate for the homography using all of the matches previously esti-
+  mated as inliers.
+  '''
+
   return H, inlier_ind
--- stitching.py
+++ stitching.py
@@ -5,25 +5,16 @@
 # libs
 import numpy as np
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-from skimage.feature import corner_harris
 import os
 from scipy import signal
 from PIL import Image
-=======
-import os
-from scipy import signal
-from PIL import Image
-
 from skimage.feature import corner_peaks, peak_local_max
->>>>>>> 4bd9615e70cfc4a776d8c6d28878686da6106121
 
-from skimage.feature import corner_peaks, peak_local_max
 # files
 from corner_detector import *
 from anms import *
 from feat_desc import *
-from feat_match import *
+# from feat_match import *
 
 '''
   Convert RGB image to gray one manually
@@ -54,12 +45,15 @@ def stitching():
     print("Running corner detection...")
     corner_matrix1 = corner_detector(rgb2gray(image1))
     corner_matrix2 = corner_detector(rgb2gray(image2))
-    # print(corner_matrix1)
+    #print(corner_matrix1)
 
     # for now just do this
     print("Non-maximal suppression...")
-    corners1 = corner_peaks(corner_matrix1, min_distance=3).transpose()
-    corners2 = corner_peaks(corner_matrix2, min_distance=3).transpose()
+    max_pts = 100
+    corners1 = anms(corner_matrix1, max_pts)
+    corners2 = anms(corner_matrix2, max_pts)
+    # corners1 = corner_peaks(corner_matrix1, min_distance=3).transpose()
+    # corners2 = corner_peaks(corner_matrix2, min_distance=3).transpose()
     # print("corners1", corners1)
     # print("corners2", corners2)
 
@@ -67,11 +61,11 @@ def stitching():
     # Display image
     plt.subplot(121)
     plt.imshow(image1)
-    plt.plot(corners1[1], corners1[0], 'ro')
+    plt.plot(corners1[0], corners1[1], 'ro')
 
     plt.subplot(122)
     plt.imshow(image2)
-    plt.plot(corners2[1], corners2[0], 'ro')
+    plt.plot(corners2[0], corners2[1], 'ro')
 
     plt.show()
 
