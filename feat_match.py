c8e2a04c57c5d67f5235fc761a91ee859b22fb86
mchiquier <mchiquier@gmail.com>
2018-11-01 12:40:11 -0700

    fixing a bug

--- feat_match.py
+++ feat_match.py
@@ -10,8 +10,9 @@
     Remember to filter the correspondences using the ratio of the best and second-best match SSD. You can set the threshold to 0.6.
     - Input descs1: 64 × N1 matrix representing the corner descriptors of first image.
     - Input descs2: 64 × N2 matrix representing the corner descriptors of second image.
-    - Outpuy match: N1 × 1 vector where match i points to the index of the descriptor in descs2 that matches with the
-                    feature i in descriptor descs1. If no match is found, you should put match i = −1.
+    - Outpuy match: N1 × 1 vector where match i points to the index of 
+    the descriptor in descs2 that matches with the
+    feature i in descriptor descs1. If no match is found, you should put match i = −1.
 '''
 
 import numpy as np
@@ -60,7 +61,7 @@ def feat_match(descs1, descs2):
     '''Check that both are greater than a threshold and if so adding that pair of featuers'''
     if ratio < 0.7 and ratios < 0.7:
         #take index_of_best, index_of_second_best,
-      pairs.append((index_of_best[0], index_of_second_best[0]))
+      pairs.append(index_of_best[0])
     return pairs
 
 a= np.array([[5,4,3],[10,9,1]])
